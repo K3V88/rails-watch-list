58aae8e3d52290efe5388503f059a3d6610e3650
kevin stal <kevin.stal@gmx.ch>
2023-08-25 12:13:22 +0200

    Add dotenv-rails

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,23 +1,25 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
-    @bookmark = @list.bookmarks.create(bookmark_params)
+    @bookmark.list = @list
 
     if @bookmark.save
-      redirect_to list_path(@list), notice: 'Bookmark was successfully created.'
+      redirect_to list_path(@list), notice: 'Bookmark was successfully created'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully destroyed'
   end
 
   private
